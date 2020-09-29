fdc61ed216735a7a79e774971aca672704ed7ac9
Eduardo Oliveira <eduardomoliveira@apoioprodesp.sp.gov.br>
2020-09-28 21:27:31 -0300

    Adicionando a migração de tarifas e habilitando o login de usuário.

---
+++ database/migrations/2020_09_28_235135_create_tarifas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTarifasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarifas', function (Blueprint $table) {
+            $table->id();
+            $table->date('data_ini')->nullable();
+            $table->date('data_fim')->nullable();
+            $table->decimal('preco',8,2);
+            $table->string('estoque',255);
+            $table->enum('disponivel',['S','N'])->default('S');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarifas');
+    }
+}
